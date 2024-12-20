33aeb3ccc02c1982a9fde21150b182fe9e1fd0fa
Sam Ellard <sam.ellard@gmail.com>
2024-12-20 19:01:35 +0900

    Updated index and added sorting to scope

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,39 +1,45 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
+  setup do
+    @draft_post = BlogPost.new(published_at: nil)
+    @published_post = BlogPost.new(published_at: 1.month.ago)
+    @scheduled_post = BlogPost.new(published_at: 1.month.from_now)
+  end
+
   test "draft? returns true for draft blog post" do
-    assert BlogPost.new(published_at: nil).draft?
+    assert @draft_post.draft?
   end
 
   test "draft? returns false for published blog post" do
-    refute BlogPost.new(published_at: 1.month.ago).draft?
+    refute @published_post.draft?
   end
 
   test "draft? returns false for scheduled blog post" do
-    refute BlogPost.new(published_at: 1.month.from_now).draft?
+    refute @scheduled_post.draft?
   end
 
   test "published? returns true for published blog post" do
-    assert BlogPost.new(published_at: 1.month.ago).published?
+    assert @published_post.published?
   end
 
   test "published? returns false for draft blog post" do
-    refute BlogPost.new(published_at: nil).published?
+    refute @draft_post.published?
   end
 
   test "published? returns false for scheduled blog post" do
-    refute BlogPost.new(published_at: 1.year.from_now).published?
+    refute @scheduled_post.published?
   end
 
-  test "scheduled? returns true for scheduled post" do
-    assert BlogPost.new(published_at: 1.year.from_now).scheduled?
+  test "scheduled? returns true for scheduled blog post" do
+    assert @scheduled_post.scheduled?
   end
 
   test "scheduled? returns false for draft blog post" do
-    refute BlogPost.new(published_at: nil).scheduled?
+    refute @draft_post.scheduled?
   end
 
-  test "scheduled returns false for published blog post" do
-    refute BlogPost.new(published_at: 7.years.ago).scheduled?
+  test "scheduled? returns false for published blog post" do
+    refute @published_post.scheduled?
   end
 end
